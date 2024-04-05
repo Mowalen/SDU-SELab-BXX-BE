b2105f5e7801cd79b0ad1680e73c24058fb257ac
Fetter <93355839+159753Fetter@users.noreply.github.com>
2024-04-05 16:37:22 +0800

    用户表中增加邮箱，店铺表中增加图片

---
+++ model/model/db_init.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+
+from const import SQLALCHEMY_DATABASE_URL,SQLALCHEMY_DATABASE_URL_MASTER
+
+# 这里需要引入所有使用 Base 的 Model
+from Model.user import *
+
+create_table_list = [User, Session, Shop, Product, Order, Comment
+                     ]
+
+if __name__ == "__main__":
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    for tb in create_table_list:
+        tb.__table__.create(bind=engine, checkfirst=True)
+
