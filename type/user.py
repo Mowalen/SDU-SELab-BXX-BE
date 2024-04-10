6a6bf6e1ccc0c2d4ac667534e929fac92edca6a2
Fetter <93355839+159753Fetter@users.noreply.github.com>
2024-04-10 16:31:28 +0800

    修改个人信息接口

--- type/user.py
+++ type/user.py
@@ -1,8 +1,9 @@
 import hashlib
 from datetime import datetime, date
-from typing import Any, Union, Literal
+from typing import Any, Union, Literal, Optional
 
 import typing_extensions
+from fastapi import UploadFile, File, Form
 from pydantic import BaseModel, ConfigDict, Field
 
 
@@ -17,10 +18,10 @@ class register_interface(login_interface): # 注册时使用
     type: int = None
 
 class user_edit_interface(register_interface): # 用户登录成功后修改个人信息
-    address: str = None
-    phone: str = None
-    id_card_number: str = None
-    photo: str = None
+    address: str = Form(None)
+    phone: str = Form(None)
+    id_card_number: str = Form(None)
+    photo: UploadFile = File(None)
     token: str = None
 
 class user_add_interface(BaseModel): # 向数据库中加入个人信息
@@ -44,9 +45,30 @@ class session_interface(BaseModel): # 用于token缓存
 
 class order_interface(BaseModel): # 用于返回商品订单详情
     shop_name: str = None
-    status: str = None
-    amount: str = None
-    quantity: str = None
+    status: int = 0
+    amount: float = 0.0
+    quantity: int = 0
     name: str = None
     photo: str = None
     address: str = None
+    order_id: int = 0
+    shop_id: int = 0
+    product_id: int = 0
+    time: datetime = None
+
+class shop_interface(BaseModel): # 用于返回店铺详情
+    shop_name: str = None
+    sales_volume: int = 0
+    address: str = None
+    photo: str = None
+    time: str = None
+    order: list = []
+
+class shop_id_interface(BaseModel): #用户返回店铺id
+    id: int = 0
+
+class shop_create_interface(BaseModel): #商家创建店铺
+    address: str = Form(None)
+    name: str = Form(None)
+    user_id: int = 0
+    photo: UploadFile = File(None)
