9c9f6cd82cd6475f835c609d046695be14d07d83
yxpoxy <1808877600@qq.com>
2024-06-05 19:19:22 +0800

    6.3

--- test.py
+++ test.py
@@ -1,28 +1,16 @@
-from pydantic import BaseModel
+import requests
+import json
 
-class User(BaseModel):
-    name: str
-    age: int
+url = "http://23.95.222.103:8080/wordscheck"
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+data = {
+    "content": "他在传播艳情内容"
+}
 
-user = User(name="John", age=25)
+response = requests.post(url, headers=headers, data=json.dumps(data))
 
-# 使用model_dump()方法返回一个字典
-dumped_data = user.model_dump()
-
-print(dumped_data)
-# 输出：{'name': 'John', 'age': 25}
-
-# 使用.dict()方法返回一个字典，包含准确的类型信息
-dict_data = user.dict()
-
-print(dict_data)
-# 输出：{'name': 'John', 'age': 25}
-
-# 打印每个字段的类型
-for field_name, field_value in dict_data.items():
-    field_type = user.__annotations__[field_name]
-    print(f"{field_name}: {field_type}")
-
-# 输出：
-# name: <class 'str'>
-# age: <class 'int'>
+print("Status Code:", response.status_code)
+print("Response Body:", response.json()["return_str"])
